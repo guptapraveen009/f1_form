44c5b0bd35927623ec2490ba594d665cce21cb91
Gupta Praveen Kumar <149958810+guptapraveen009@users.noreply.github.com>
2026-09-24 19:45:17 +0530

    Update

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 let btn = document.getElementById("button");
+let name = document.getElementById("name");
+let ph = document.getElementById("ph-no");
+let email = document.getElementById("email");
+let text = document.getElementById("textarea");
 btn.addEventListener("click" , () => {
     alert("Thank You For Your FeedBack")
 })
